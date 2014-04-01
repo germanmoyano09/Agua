ef95571b2ff517e25c250ab6e98d7b2f91a4eff3
German Moyano <c.germanmoyano@gmail.com>
2014-04-01 01:23:19 -0300

    Modifico menu terapias

--- fijo/menu.php
+++ fijo/menu.php
@@ -28,20 +28,74 @@
 					echo '<a href="#">TERAPIAS</a>';
 				}
 			?>
-				<ul>                                  
-					<li><a href="terapia_jahara_technique.php">JAHARA</a></li>
-					<li><a href="terapia_watsu.php">WATSU</a></li>
-					<li><a href="terapia_aquarelax.php">AQUARELAX</a></li>
-					<li><a href="terapia_craneosacral.php">CRANEOSACRAL</a></li>
-					<li><a href="terapia_experiencia_gong.php">EXPERIENCIA GONG</a></li>
-					<li><a href="terapia_con_cuencos.php">TERAPIA CON CUENCOS</a></li>
-					<li><a href="terapia_monochord.php">MONOCHORD</a></li>
-					<li><a href="terapia_ai_chi.php">AI CHI</a></li>
+			<ul>
+				<?php
+					if(isset($subtitulo)){
+						if($subtitulo === "jahara"){
+							echo '<li><a class="sel_terapia" href="#">JAHARA</a></li>';
+						}else{
+							echo '<li><a href="terapia_jahara_technique.php">JAHARA</a></li>';
+						}
+						
+						if($subtitulo === "watsu"){
+							echo '<li><a class="sel_terapia" href="#">WATSU</a></li>';
+						}else{
+							echo '<li><a href="terapia_watsu.php">WATSU</a></li>';
+						}
+						
+						if($subtitulo === "aquarelax"){
+							echo '<li><a class="sel_terapia" href="#">AQUARELAX</a></li>';
+						}else{
+							echo '<li><a href="terapia_aquarelax.php">AQUARELAX</a></li>';
+						}
+						
+						if($subtitulo === "craneosacral"){
+							echo '<li><a class="sel_terapia" href="#">CRANEOSACRAL</a></li>';
+						}else{
+							echo '<li><a href="terapia_craneosacral.php">CRANEOSACRAL</a></li>';
+						}
+						
+						if($subtitulo === "experiencia_gong"){
+							echo '<li><a class="sel_terapia" href="#">EXPERIENCIA GONG</a></li>';
+						}else{
+							echo '<li><a href="terapia_experiencia_gong.php">EXPERIENCIA GONG</a></li>';
+						}
+						
+						if($subtitulo === "con_cuencos"){
+							echo '<li><a class="sel_terapia" href="#">TERAPIA CON CUENCOS</a></li>';
+						}else{
+							echo '<li><a href="terapia_con_cuencos.php">TERAPIA CON CUENCOS</a></li>';
+						}
+						
+						if($subtitulo === "monochord"){
+							echo '<li><a class="sel_terapia" href="#">MONOCHORD</a></li>';
+						}else{
+							echo '<li><a href="terapia_monochord.php">MONOCHORD</a></li>';
+						}
+						
+						if($subtitulo === "ai_chi"){
+							echo '<li><a class="sel_terapia" href="#">AI CHI</a></li>';
+						}else{
+							echo '<li><a href="terapia_ai_chi.php">AI CHI</a></li>';
+						}
+					}else{
+				?>	
+						<li><a href="terapia_jahara_technique.php">JAHARA</a></li>
+						<li><a href="terapia_watsu.php">WATSU</a></li>
+						<li><a href="terapia_aquarelax.php">AQUARELAX</a></li>
+						<li><a href="terapia_craneosacral.php">CRANEOSACRAL</a></li>
+						<li><a href="terapia_experiencia_gong.php">EXPERIENCIA GONG</a></li>
+						<li><a href="terapia_con_cuencos.php">TERAPIA CON CUENCOS</a></li>
+						<li><a href="terapia_monochord.php">MONOCHORD</a></li>
+						<li><a href="terapia_ai_chi.php">AI CHI</a></li>
 					
-					<!--
+						<!--
 						<li><a href="single_post.php"></a></li>
-					-->
-				</ul>
+						-->
+				<?php	
+					}
+				?>	
+			</ul>
 		</li>
 		
 		<li>
